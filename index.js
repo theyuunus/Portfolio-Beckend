4dae3f00f1c1e07d2641b642e0513693fd903878
Yunus <128239091+theyuunus@users.noreply.github.com>
2024-03-13 18:00:05 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(router);
+server.use(middlewares)
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
